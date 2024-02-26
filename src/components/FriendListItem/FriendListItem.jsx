85906e9b079267ebf7bffc0942783d70d433d20d
OlhaKatrych <olgak198931@gmail.com>
2024-02-26 16:49:23 +0200

    friends completed

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,10 +1,15 @@
-import css from "./FriendListItem.module.css"
+import css from "./FriendListItem.module.css";
+import clsx from "clsx";
+
 function FriendListItem({ avatar, name, isOnline }) {
   return (
-    <div>
-      <img src={avatar} alt="Avatar" width="48" />
-      <p>{name}</p>
-      <p>{isOnline}</p>
+    <div className={css.cardWrapper}>
+      <img src={avatar} alt="Avatar" width="48" className={css.cardImg} />
+      <p className={css.cardText}>{name}</p>
+      <p className={clsx(css.cardText, {
+        [css.cardOnline]: isOnline === true,
+        [css.cardOffline]: isOnline === false,
+      })}>{isOnline ? "Online" : "Offline"}</p>
     </div>
   );
 }
