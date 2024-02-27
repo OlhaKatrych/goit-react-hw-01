517f1ed8d55d7ad4f138d4ab80698925f4b46380
OlhaKatrych <olgak198931@gmail.com>
2024-02-27 11:02:29 +0200

    transactions completed

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,30 +1,31 @@
 import css from "./TransactionHistory.module.css";
-
+import clsx from "clsx";
 function TransactionHistory({ items }) {
-  console.log(items);
+  const toggleBackgroundColor = true;
   return (
     <div className={css.tableContainer}>
-    <table className={css.tableFlex}>
-      <thead className={css.tTable}>
-        <tr className={css.trTable}>
-          <th>Type</th>
-          <th>Amount</th>
-          <th>Currency</th>
-        </tr>
-      </thead>
-      {items.map((item) => {
-        return (
-          <tbody key={item.id} className={css.bodyTable}>
-            <tr>
-              <td>{item.type}</td>
-              <td>{item.amount}</td>
-              <td>{item.currency}</td>
-            </tr>
-          </tbody>
-          
-        );
-      })}
-    </table>
+      <table className={css.tableFlex}>
+        <thead className={css.headTable}>
+          <tr className={css.trTable}>
+            <th className={css.th}>Type</th>
+            <th className={css.th}>Amount</th>
+            <th className={css.th}>Currency</th>
+          </tr>
+        </thead>
+        {items.map((item) => {
+          return (
+            <tbody key={item.id} className={clsx(css.bodyTable, {
+              [css.trColor]: toggleBackgroundColor,
+            })}>
+              <tr className={css.trWrapper}>
+                <td className={css.td}>{item.type}</td>
+                <td className={css.td}>{item.amount}</td>
+                <td className={css.td}>{item.currency}</td>
+              </tr>
+            </tbody>
+          );
+        })}
+      </table>
     </div>
   );
 }
